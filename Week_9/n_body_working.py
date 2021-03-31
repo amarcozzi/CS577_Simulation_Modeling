f3778b8e9df010596fb3fe3af8a51617e9001206
anthony <anthonymarcozzi@gmail.com>
2021-03-31 12:08:07 -0600

    n body

--- Week_9/n_body_working.py
+++ Week_9/n_body_working.py
@@ -38,7 +38,7 @@ def n_body(t, y, p):
             rij = pos_matrix[i, :] - pos_matrix[j, :]
 
             # Compute the force from body j onto body i
-            Fij = (-p['G'] * p['m'][i] * p['m'][j] / np.power(np.linalg.norm(rij), 3)) * rij
+            Fij = p['force'](p['G'], p['m'][i], p['m'][j], rij)
 
             # Fill in the symmetric pieces of the force matrix
             Fmatrix[i, j, :] = Fij / p['m'][i]
@@ -59,69 +59,13 @@ def n_body(t, y, p):
 
     return dxdt
 
+def gravitational(G, m1, m2, r):
+    """ Computes the gravitational force between two bodies with large mass """
+    return (-G * m1 * m2 / np.power(np.linalg.norm(r), 3)) * r
 
-""" OLD FORCE FUNCTION
-    def force_function(vec, m1, m2, G):
-    f = -G * m1 * m2 / np.power(np.linalg.norm(vec), 3)
-    return f"""
-
-"""
-TEST THE N BODY FUNCTION
-"""
-"""y_sample = np.array([1.382857, 0,
-                     0, 0.157030,
-                     -1.382857, 0,
-                     0, -0.157030,
-                     0, 0.584873,
-                     1.871935, 0,
-                     0, -0.584873,
-                     -1.871935, 0], dtype=np.float128)
-
-euler = np.array([0, 0, 1, 0, -1, 0, 0, 0, 0, .8, 0, -.8])
-four_body = np.array([1.382857, 0,
-                      0, 0.157030,
-                      -1.382857, 0,
-                      0, -0.157030,
-                      0, 0.584873,
-                      1.871935, 0,
-                      0, -0.584873,
-                      -1.871935, 0], dtype=np.float128)
-helium_1 = np.array([0, 0, 2, 0, -1, 0, 0, 0, 0, .95, 0, -1])
-
-p = {'m': [1, 1, 1], 'G': 1, 'dimension': 2, 'fix_first': False}
-p4 = {'m': [1, 1, 1, 1], 'G': 1, 'dimension': 2, 'fix_first': False}
-phe = {'m': [2, -1, -1], 'G': 1, 'dimension': 2, 'fix_first': True}
-
-headings = ['RUN', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'vx1', 'vy1', 'vx2', 'vy2', 'vx3', 'vy3']
-t = PrettyTable(headings)
-t.add_row(['euler'] + list(n_body(0, euler, p)))
-t.add_row(['He'] + list(n_body(0, helium_1, phe)))
-print(t)
-
-headings = ['RUN', 'x1', 'y1', 'x2', 'y2', 'x3', 'y3', 'x4', 'y4', 'vx1', 'vy1', 'vx2', 'vy2', 'vx3', 'vy3', 'vx4',
-            'vy4']
-t = PrettyTable(headings)
-t.add_row(['4 body'] + list(n_body(0, four_body, p4)))
-print(t)"""
-
-""" ODE LIMITATIONS SECTION """
-"""euler = np.array([0, 0, 1, 0, -1, 0, 0, 0, 0, .8, 0, -.8])
-
-montgomery = np.array([0.97000436, -0.24308753, -0.97000436, 0.24308753, 0., 0.,
-                       0.466203685, 0.43236573, 0.466203685, 0.43236573,
-                       -0.93240737, -0.86473146])
-lagrange = np.array([1., 0., -0.5, 0.866025403784439, -0.5, -0.866025403784439,
-                     0., 0.8, -0.692820323027551, -0.4, 0.692820323027551, -0.4])
-
-p3 = {'m': [1, 1, 1], 'G': 1, 'dimension': 2, 'fix_first': False}
-
-ode_solver = ODESolver()
-tspan = [0, 100]
-t, y = ode_solver.solve_ode(n_body, tspan, euler, ode_solver.EulerRichardson, p3, first_step=1)"""
 
 euler      = np.array([0,0,1,0,-1,0,0,0,0,.8,0,-.8])
-# p3 = {'m':[1,1,1],'G':1,'dimension':2,'force':gravitational,'fix_first':False}
-p3 = {'m':[1,1,1],'G':1,'dimension':2,'fix_first':False}
+p3 = {'m':[1,1,1],'G':1,'dimension':2,'force':gravitational,'fix_first':False}
 
 y0 = euler
 p  = p3
@@ -130,4 +74,4 @@ dt = 0.0025  # This is wrong - figure it out!
 t_span = [0,100]
 
 solver = ODESolver()
-t_s,y = solver.solve_ode(n_body,t_span, y0, solver.RK45, p,first_step=dt, atol=1e-6, rtol=1e-6, S=0.98)
+t_s,y = solver.solve_ode(n_body,t_span, y0, solver.RK45, p,first_step=dt, atol=1e-6, rtol=1e-6, S=0.9)
--- Week_9/ode_solver.py
+++ Week_9/ode_solver.py
@@ -11,21 +11,21 @@ class ODESolver:
         """ Computes the change in state via the Euler algorithm """
         y_computed = f(t, y, *args) * dt + y
         self.num_f_calls += 1  # Increment the number of function calls
-        return y_computed
+        return y_computed, dt
 
     def EulerCromer(self, y, dt, f, t, *args, **options):
         """ Computes the change in state via the Euler-Cromer method """
         y_end = self.Euler(y, dt, f, t, *args)
         y_return = f(t + dt, y_end, *args) * dt + y
         self.num_f_calls += 1  # Increment the number of function calls
-        return y_return
+        return y_return, dt
 
     def EulerRichardson(self, y, dt, f, t, *args, **options):
         """ Computes the change in state via the Euler-Richardson method """
         y_mid = self.Euler(y, dt / 2, f, t, *args)
         y_computed = f(t + dt / 2, y_mid, *args) * dt + y
         self.num_f_calls += 1  # Increment the number of function calls
-        return y_computed
+        return y_computed, dt
 
     def RungeKutta4(self, y, dt, f, t, *args, **options):
         """ Computes the change in state via the fourth-order Runge-Kutta integration method """
@@ -35,39 +35,54 @@ class ODESolver:
         k4 = dt * f(t + 0.5 * dt, y + k3, *args)
 
         self.num_f_calls += 4  # Increment the number of function calls
-        return y + (1 / 6) * k1 + (1 / 3) * k2 + (1 / 3) * k3 + (1 / 6) * k4
+        return y + (1 / 6) * k1 + (1 / 3) * k2 + (1 / 3) * k3 + (1 / 6) * k4, dt
 
     def RK45(self, y, dt, f, t, *args, **options):
         """ Computes the change in state via the fifth-order Runge-Kutta integration method """
         # Define constant weights
-        b = np.array([35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84, 0])
-        b_star = np.array([5179 / 57600, 0, 7571 / 16695, 393 / 640, -92097 / 339200, 187 / 2100, 1 / 40])
-        c = np.array([1 / 5, 3 / 10, 4 / 5, 8 / 9, 1, 1])
-        a = np.array([[0, 0, 0, 0, 0, 0],
-                      [1 / 5, 0, 0, 0, 0, 0],
-                      [3 / 40, 9 / 40, 0, 0, 0, 0],
-                      [44 / 45, -56 / 15, 32 / 9, 0, 0, 0],
-                      [19372 / 6561, -25360 / 2187, 64448 / 6561, -212 / 729, 0, 0],
-                      [9017 / 3168, -355 / 33, 46732 / 5247, 49 / 176, -5103 / 18656, 0],
-                      [35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84]])
+        b = options['b']
+        b_star = options['b_star']
+        c = options['c']
+        c2, c3, c4, c5 = 0.2, 0.3, 0.8, 8.0 / 9.0
+        a = options['a']
+
+        k_test = np.zeros([7, y.size])
+
+        """
+        while t < t_0 + delta t_0_r:
+        for i in range(1, 7):
+            yt = y0.copy() + np.dot(a[i], k) * dt
+            k[i] = f(t + c[i]*dt, yt, *args)
+        y_5 = dot(b, k_6)
+        y_4 = dot(b_start
+        """
 
         # compute the k terms for RK5
-        k = np.zeros([6, y.size])
         k1 = dt * f(t, y, *args)
-        k2 = dt * f(t + c[1] * dt, y + a[1, 0] * k1, *args)
-        k3 = dt * f(t + c[2] * dt, y + a[2, 0] * k1 + a[2, 1] * k2, *args)
-        k4 = dt * f(t + c[3] * dt, y + a[3, 0] * k1 + a[3, 1] * k2 + a[3, 2] * k3, *args)
-        k5 = dt * f(t + c[4] * dt, y + a[4, 0] * k1 + a[4, 1] * k2 + a[4, 2] * k3 + a[4, 3] * k4, *args)
-        k6 = dt * f(t + c[5] * dt, y + a[5, 0] * k1 + a[5, 1] * k2 + a[5, 2] * k3 + a[5, 3] * k4 + a[5, 4] * k5, *args)
+        k2 = dt * f(t + c2 * dt, y + a[1, 0] * k1, *args)
+        k3 = dt * f(t + c3 * dt, y + a[2, 0] * k1 + a[2, 1] * k2, *args)
+        k4 = dt * f(t + c4 * dt, y + a[3, 0] * k1 + a[3, 1] * k2 + a[3, 2] * k3, *args)
+        k5 = dt * f(t + c5 * dt, y + a[4, 0] * k1 + a[4, 1] * k2 + a[4, 2] * k3 + a[4, 3] * k4, *args)
+        k6 = dt * f(t + dt, y + a[5, 0] * k1 + a[5, 1] * k2 + a[5, 2] * k3 + a[5, 3] * k4 + a[5, 4] * k5, *args)
+        k7 = dt * f(t + dt, y + a[6, 0] * k1 + a[6, 1] * k2 + a[6, 2] * k3 + a[6, 3] * k4 + a[6, 4] * k5+ a[6, 5] * k6, *args)
         self.num_f_calls += 6  # Increment the number of function calls
-        y_new = y + b[0]*k1 + b[1]*k2 + b[2]*k3 + b[3]*k4 + b[4]*k5 + b[5]*k6
+        y_new = y + b[0] * k1 + b[1] * k2 + b[2] * k3 + b[3] * k4 + b[4] * k5 + b[5] * k6
+        k_array = np.row_stack([k1, k2, k3, k4, k5, k6, k7])
+        y_new_test = y + np.dot(b, k_array)
+
+        # compute the error between the 4th and 5th order approximations
+        b_diff = b - b_star
+        delta = b_diff[0] * k1 + b_diff[1] * k2 + b_diff[2] * k3 + b_diff[3] * k4 + b_diff[4] * k5 + b_diff[5] * k6
+        delta_test = np.dot(b - b_star, k_test)
+        # This works too
+        scale = options['atol'] + options['rtol'] * np.maximum(np.abs(y), np.abs(y_new_test))
+        # scale = options['atol'] + options['rtol'] * np.abs(y_new)
+        # err = np.sqrt(np.mean((delta / scale) ** 2))
+        err = np.sqrt(np.mean((delta_test / scale) ** 2))
 
         # Compute the new time step, h_new, based on errors between 4th and 5th order Runge-Kutta methods
-        b_diff = b - b_star
-        delta = b_diff[0]*k1 + b_diff[1]*k2 + b_diff[2]*k3 + b_diff[3]*k4 + b_diff[4]*k5 + b_diff[5]*k6
-        scale = options['atol'] + options['rtol'] * np.maximum(np.abs(y), np.abs(y_new))
-        err = np.sqrt(np.mean((delta / scale) ** 2))
-        dt_new = options['S'] * dt * np.power(1/err, 1/5)
+        # dt_new = options['S'] * dt * np.power(1 / err, 1 / 5)
+        dt_new = options['S'] * dt * np.power(1 / err, 1 / 5)
 
         # Limit the up and down scaling of the time step
         if dt_new > dt:
@@ -75,6 +90,9 @@ class ODESolver:
         else:
             dt_new = max(dt_new, dt / 5)
 
+        # reject the step and try again with a smaller dt
+        if err > 1:
+            y_new, dt_new = self.RK45(y, dt_new, f, t, *args, **options)
 
         return y_new, dt_new
 
@@ -132,6 +150,7 @@ class ODESolver:
         """
         # Reset the number of function calls
         self.num_f_calls = 0
+        self.FSAL = 0
 
         # pull in solver parameters from passed arguments
         dt = options['first_step']
@@ -139,15 +158,32 @@ class ODESolver:
         tf = tspan[1]
         y = [y0]
         t = [t0]
+
+        # Check if the method call is RK45, if so, pack the options with the Dormand-Prince coefficients
+        if method == self.RK45:
+            options['b'] = np.array([35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84, 0])
+            options['b_star'] = np.array(
+                [5179 / 57600, 0, 7571 / 16695, 393 / 640, -92097 / 339200, 187 / 2100, 1 / 40])
+            options['c'] = np.array([1, 1 / 5, 3 / 10, 4 / 5, 8 / 9, 1, 1])
+            options['a'] = np.array([[0, 0, 0, 0, 0, 0],
+                                     [1 / 5, 0, 0, 0, 0, 0],
+                                     [3 / 40, 9 / 40, 0, 0, 0, 0],
+                                     [44 / 45, -56 / 15, 32 / 9, 0, 0, 0],
+                                     [19372 / 6561, -25360 / 2187, 64448 / 6561, -212 / 729, 0, 0],
+                                     [9017 / 3168, -355 / 33, 46732 / 5247, 49 / 176, -5103 / 18656, 0],
+                                     [35 / 384, 0, 500 / 1113, 125 / 192, -2187 / 6784, 11 / 84]])
+
         while t[-1] < tf:
             current_t = t[-1]
             # Compute change in t and change in position y at each time step
-            if method == 'RK45':
+            """if method == self.RK45:
                 new_y, dt = method(y[-1], dt, f, t[-1], *args, **options)
             else:
-                new_y = method(y[-1], dt, f, t[-1], *args, **options)
+                new_y = method(y[-1], dt, f, t[-1], *args, **options)"""
+            new_y, dt = method(y[-1], dt, f, t[-1], *args, **options)
             y.append(new_y)
             t.append(t[-1] + dt)
 
+        print(f'The force function was called {self.num_f_calls} times.')
         # Convert t, y to np arrays and return them
         return np.array(t), np.array(y)
